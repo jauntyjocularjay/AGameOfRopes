2d132ada3158e094b194224ed875ac7d1a9523c3
ShroomPoof <thatsasecret>
2025-02-19 21:16:05 -0500

    tried to add sound effects to the Unity Scene

--- Assets/Scripts/CharacterData.cs
+++ Assets/Scripts/CharacterData.cs
@@ -5,6 +5,9 @@ public class CharacterData : ScriptableObject
 {
     public int maxWill = 120;
     public int maxBindings = 120;
+    public AudioSource a_Attack;
+    public AudioSource a_Guard;
+    public AudioSource a_Struggle;
     public int damage;
     public int bind;
     public int specialMultiplier;
